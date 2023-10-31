9edda1a61e535cadff9576b6f92c3ee250ef1184
unknown <henryifeanyi9933@gmail.comgit config --golbal user.name ifeanyi>
2023-10-31 16:34:22 +0100

    my vita project

--- main.js
+++ main.js
@@ -14,3 +14,15 @@ navToggle.addEventListener('click', ()=> {
 
     primaryHeader.toggleAttribute('data-overlay');
 });
+
+const slider = new A11YSlider(document.querySelector(".slider"), {
+    adaptiveHeight: false,
+    dots: true,
+    centerMode: true,
+    arrows: false,
+    responsive: {
+      480: {
+        dots: false, // dots enabled 1280px and up
+      },
+    },
+  });
